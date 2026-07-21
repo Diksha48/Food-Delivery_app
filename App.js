9e15edc0795663132089d77313c6e56cadb8ba41
Diksha48 <80918851+Diksha48@users.noreply.github.com>
2026-07-21 21:14:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <hr/>
+      <div className='app-content'>
+        <Sidebar/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
